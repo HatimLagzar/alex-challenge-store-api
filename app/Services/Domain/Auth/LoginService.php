75f4f89aba7a534ef767d94a9b60d220dfd4160f
Hatim Lagzar <lhatimdev@gmail.com>
2022-10-07 00:41:46 +0100

    remove uncessary method

--- app/Services/Domain/Auth/LoginService.php
+++ app/Services/Domain/Auth/LoginService.php
@@ -2,9 +2,7 @@
 
 namespace App\Services\Domain\Auth;
 
-use App\Models\User;
 use App\Services\Domain\Auth\Exceptions\IncorrectCredentialsException;
-
 use function auth;
 
 class LoginService
@@ -24,17 +22,5 @@ class LoginService
         }
 
         return $token;
-    }
-
-    public function login(string $email, string $password): bool
-    {
-        return auth()->attempt(
-            [
-                User::EMAIL_COLUMN    => $email,
-                User::PASSWORD_COLUMN => $password,
-                User::ROLE_COLUMN     => User::ADMIN_ROLE,
-            ],
-            true
-        );
     }
 }
